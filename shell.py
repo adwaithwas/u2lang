94cd15cd936073af90667c3127b28cd4d971e52b
adwaithwas <betteradwaith@gmail.com>
2024-12-30 07:35:30 +0530

    added position class for betetr error display message and added an exit option in shell.py

--- shell.py
+++ shell.py
@@ -1,9 +1,13 @@
 import u2lang as u2
 
 while True:
-    text = input('uulang > ')
+    text = input('~~ ')
     result, error = u2.run(text)
 
+    if text == 'u2.exit':
+        print('Thank you for Trying u2lang :)')
+        break
+    
     if error:
         print(error.print_error())
     else: print(result)
--- u2lang.py
+++ u2lang.py
@@ -1,18 +1,42 @@
 #ERRORS
 class Errors:
-    def __init__(self, error_name, details):
+    def __init__(self, error_name, details, pos_start, pos_end):
         self.error_name = error_name
         self.details = details
+        self.pos_start = pos_start
+        self.pos_end = pos_end
 
     def print_error(self):
-        result = f'{self.error_name}: {self.details}'
+        result = f'{self.error_name}: {self.details}, at (line:{self.pos_start.ln+1} :: col:{self.pos_start.col+1})'
         return result
 
 class IllegalCharacterError(Errors):
-    def __init__(self, details):
-        super().__init__('Illegal Character', details)
+    def __init__(self, details, pos_start, pos_end):
+        super().__init__('Illegal Character', details, pos_start, pos_end)
 
-        
+    
+
+#POSITIONS
+class Position:
+    def __init__(self, idx, ln, col):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+    
+    def advance(self, current_char):
+        self.idx += 1
+        self.col += 1
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+
+        return self
+    
+    def copy(self):
+        return Position(self.idx, self.ln, self.col)
+
+
+
 
 #TOKENS
 TT_PLUS = 'PLUS'
@@ -36,19 +60,20 @@ class Token:
         else:
             return f'{self.type}'
         
+        
 
 class Lexer:
     def __init__(self, text):
         self.text = text
-        self.pos = -1
+        self.pos = Position(-1, 0, -1)
         self.current_char = None
         self.advance()
         self.errors_list = []
 
     def advance(self):
-        self.pos += 1
+        self.pos.advance(current_char = self.current_char)
 
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     def make_number(self):
         numStr = ''
@@ -98,10 +123,12 @@ class Lexer:
                 tokens.append(Token(TT_LPAREN))
                 self.advance()
             else:
+                pos_start = self.pos.copy()
+
                 illegal_char = self.current_char
                 self.advance()
 
-                return [], IllegalCharacterError(" " + illegal_char + " ")
+                return [], IllegalCharacterError(" " + illegal_char + " ", pos_start, self.pos)
 
 
         return tokens, None
